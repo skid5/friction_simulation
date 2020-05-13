cac9a622828a91370b297568ea5b031572a0de34
Munukka Joni <joni.munukka@aalto.fi>
2020-05-13 18:57:09 +0300

    build1.py nimetty uudelleen build2.py:ksi.

--- build2/build2.py
+++ build2/build2.py
@@ -13,16 +13,20 @@ simu.create_interaction(['Au','Au'], strength=1.0, equilibrium_distance=2.375)
 simu.create_interaction(['Ag','Ag'], strength=1.0, equilibrium_distance=1.781)
 simu.create_interaction(['Au','Ag'], strength=0.1, equilibrium_distance=2.0)
 
+list_atoms()
+
 #indices
 au_indices = simu.get_indices_by_element('Au')
 ag_indices = simu.get_indices_by_element('Ag')
+bottom_indices = simu.get_indices_z_less_than(-3.5)
+top_indices = simu.get_indices_z_more_than(8.0)
 
 #dynamics and stats
 simu.create_dynamics(dt=1.0, temperature=500)
 simu.print_stats_during_simulation(interval=50.0)
 simu.save_trajectory_during_simulation(interval=50.0)
 
-simu.gather_average_force_during_simulation()
+simu.gather_average_force_during_simulation(interval=1.0, indices=[1], filename='force.txt')
 
 #happenings
 simu.fix_positions(au_indices, [True,True,True])
