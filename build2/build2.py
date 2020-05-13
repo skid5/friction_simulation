1a89bcde1802c41614e45fd91fbc8198502c92fb
Kettunen Heikki <kettunh4@brute.org.aalto.fi>
2020-05-13 19:13:07 +0300

    build2 v2

--- build2/build2.py
+++ build2/build2.py
@@ -3,10 +3,8 @@
 import  friction_tools as ft
 
 simu = ft.FrictionSimulation()
+simu.continue_from_trajectory(filename="beginning.traj")
 
-#elements
-simu.create_slab(element='Au',xy_cells=3,z_cells=2,top_z=0.0)
-simu.create_slab(element='Ag',xy_cells=4,z_cells=2,bottom_z=3.0)
 
 #interactions
 simu.create_interaction(['Au','Au'], strength=1.0, equilibrium_distance=2.375)
@@ -16,19 +14,22 @@ simu.create_interaction(['Au','Ag'], strength=0.1, equilibrium_distance=2.0)
 #indices
 au_indices = simu.get_indices_by_element('Au')
 ag_indices = simu.get_indices_by_element('Ag')
+bottom_indices = simu.get_indices_z_less_than(-3.5)
+
 
 #dynamics and stats
-simu.create_dynamics(dt=1.0, temperature=500)
+simu.create_dynamics(dt=1.0, temperature=500, coupled_indices=bottom_indices)
 simu.print_stats_during_simulation(interval=50.0)
 simu.save_trajectory_during_simulation(interval=50.0)
 
 simu.gather_average_force_during_simulation()
 
 #happenings
-simu.fix_positions(au_indices, [True,True,True])
+simu.fix_positions(bottom_indices, [True,True,True])
 simu.add_constant_force(ag_indices, [0, 0, -0.02])
-simu.fix_velocities(ag_indices, [0.005, 0, 0], [True,False,False])
-simu.set_temperature(500)
+simu.run_simulation(time=1000.0)
 
+simu.fix_velocities(ag_indices, [0.005, 0, 0], [True,False,False])
 simu.run_simulation(time=2000.0)
+
 ft.trajectory_to_xyz()
