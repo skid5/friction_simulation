ab41ba30045f7c30e010ae8d0c8a188bcb3247c5
unna <unna@Oletus.kyla.fi>
2020-05-24 13:49:46 +0300

    kansio luotu

---
+++ matlab_analyysi/constants.py
@@ -0,0 +1,12 @@
+eq_dist_iron = 2.357
+eq_dist_copper = 2.357
+eq_dist_between = 2.357
+eq_dist = 2.357
+
+bond_strength_iron = 0.1431
+bond_strength_copper = 0.1340
+bond_strength_between = 0.0693
+
+thermostat = 0.01
+
+F_min = -3.048*10**-9
